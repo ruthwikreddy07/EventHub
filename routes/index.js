3cccf89d3dbf30fcd2ec9a956bede8dbfdcaa1a0
Donthireddy Ruthwik Reddy <179065000+ruthwikreddy07@users.noreply.github.com>
2025-08-20 18:39:35 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'EVENT HUB' ,
-                      description: 'Event Hub is a modern event management and ticket booking system designed to simplify the way people discover and attend events.
-
- For Users: Browse concerts, workshops, seminars, sports, and other events with ease. Book tickets securely, receive instant e-tickets, and get event reminders so you never miss out.
-
- For Organizers/Admins: Create and publish events quickly, manage ticket pricing and availability, track real-time bookings, and analyze event performance through a centralized dashboard.
-
- Secure and Reliable: Integrated payment gateway ensures safe transactions, while automated e-ticket delivery makes the process seamless.
-
- User-Friendly: Works across devices with an intuitive interface for both event seekers and organizers.
-
-With Event Hub, managing and attending events becomes hassle-free, efficient, and engaging.'});
+                      description: 'An Event Booking System that allows users to browse events, book tickets with secure payments, and receive e-tickets. Admins can manage events, track bookings, and monitor availability in real time.'});
 });
 
 module.exports = router;
