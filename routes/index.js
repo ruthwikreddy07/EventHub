8bee01a19c75c6df449e7382248c29aa846d1473
Donthireddy Ruthwik Reddy <179065000+ruthwikreddy07@users.noreply.github.com>
2025-08-20 13:46:12 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EVENT HUB' ,
-                      description: 'An Event Booking System that allows users to browse events, book tickets with secure payments, and receive e-tickets. Admins can manage events, track bookings, and monitor availability in real time.'});
+router.get('/', function (req, res, next) {
+  res.send(`
+    <h1>EVENT HUB</h1>
+    <h2>Welcome to EVENT HUB</h2>
+    <p>
+      An Event Booking System that allows users to browse events,
+      book tickets with secure payments, and receive e-tickets.
+      Admins can manage events, track bookings, and monitor availability in real time.
+    </p>
+  `);
 });
 
 module.exports = router;
