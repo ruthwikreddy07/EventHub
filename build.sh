a2f705b33c0a1cc702f9b90a72e4f1e365f822ca
ruthwikreddy07 <179065000+ruthwikreddy07@users.noreply.github.com>
2025-10-13 23:55:49 +0530

    updating build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status
 set -o errexit
-
-echo "--- STARTING ANGULAR FRONTEND BUILD ---"
-
-# 1. Navigate to the Angular Frontend folder
 cd eventhub-ui
-
-# 2. Install ALL frontend dependencies
-echo "Installing Angular dependencies..."
-npm install 
-
-# 3. Build the Angular app for production. 
-echo "Building Angular app..."
-npx ng build --configuration production
-
-echo "--- BUILD COMPLETE ---"
+npm install
+npm run build -- --configuration production
